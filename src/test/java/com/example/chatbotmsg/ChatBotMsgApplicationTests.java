8e4c1b26edb5181542ea3c7918065901f3bc11b8
buggyfoxy <buggyfoxy0709@gmail.com>
2022-07-13 18:00:33 +0700

    chatbot

---
+++ src/test/java/com/example/chatbotmsg/ChatBotMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbotmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotMsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
